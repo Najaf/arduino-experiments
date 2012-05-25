4298ec11d7842850394e9bf555d3013765b38a96
Najaf Ali <ali.najaf@gmail.com>
2012-05-26 00:30:57 +0100

    relay

---
+++ relay/src/sketch.ino
@@ -0,0 +1,15 @@
+
+int ledPin = 2;
+
+void setup()
+{
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(ledPin, HIGH);
+  delay(1000);
+  digitalWrite(ledPin, LOW);
+  delay(1000);
+}
