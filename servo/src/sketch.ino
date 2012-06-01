d2c655868df8b9006a5e831870ec449cb4511a8e
Najaf Ali <ali.najaf@gmail.com>
2012-06-01 22:55:20 +0100

    tooling around with ma big servo

--- servo/src/sketch.ino
+++ servo/src/sketch.ino
@@ -1,12 +1,13 @@
 #include <Servo.h>
 
-int zero = 900;
+int zero = 1000;
 int ninety = 1500;
-int oneeighty = 2100;
+int oneeighty = 2000;
 
 int servoPin = 9;
 int servoPosition = 0;
-int pulseTime = ninety;
+int pulseTime = zero;
+int rotated = 0;
 
 void setup()
 {
@@ -15,8 +16,33 @@ void setup()
 
 void loop()
 {
+  if (rotated == 0) {
+    for (int i = 0; i < 100; i++ ) {
+      servoLeft();
+      delay(20);
+    }
+    rotated = 1;
+  }
+}
+
+void servoLeft()
+{
+  pulseServo(500);
+}
+
+void servoRight()
+{
+  pulseServo(2500);
+}
+
+void servoForward()
+{
+  pulseServo(1500);
+}
+
+void pulseServo(int delayTime)
+{
   digitalWrite(servoPin, HIGH);
-  delayMicroseconds(pulseTime);
+  delayMicroseconds(delayTime);
   digitalWrite(servoPin, LOW);
-  delay(25);
 }
